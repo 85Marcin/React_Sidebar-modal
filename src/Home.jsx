99711bc79268bd336bbc5db079ed8af8179caa6c
Marcin <marcinegzmont@gmail.com>
2023-09-22 12:23:40 +0100

    setting up global context

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,10 @@
 import React, { useContext } from "react"
 import { FaBars } from "react-icons/fa"
+import { useGlobalContext } from "./context"
 
 const Home = () => {
+  const { openSidebar, openModal } = useGlobalContext()
+  console.log(openSidebar)
   return <h2>home </h2>
 }
 
---
+++ src/context.jsx
@@ -0,0 +1,39 @@
+import { useState, useContext, createContext } from "react"
+
+const AppContext = createContext()
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true)
+  }
+  const closeSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+  return (
+    <AppContext.Provider
+      value={{
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar,
+        isSidebarOpen,
+        isModalOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
